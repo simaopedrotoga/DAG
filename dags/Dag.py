e19ed3e6356391ae46169e44d4adac3886584fff
Simão Machado <simaopedrotoga@gmail.com>
2024-04-23 10:19:45 +0100

    Content

--- dags/Dag.py
+++ dags/Dag.py
@@ -1,27 +1,29 @@
 # Importing packages
+import os
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.microsoft.azure.transfers.local_to_adls import LocalFilesystemToADLSOperator
 from airflow.providers.microsoft.azure.operators.synapse import AzureSynapseRunSparkBatchOperator
 
 # Defining functions to be used
 def _choose_random_customer_id(ti):
-    ti.xcom_push(key = 'my_key', value = 42) # Adjust
+    ti.xcom_push(key = "my_key", value = "42") # Adjust
 
-def _tst(ti):
-    user = ti.xcom_pull(key = 'my_key', task_ids = 'choose_random_customer_id')
-    user = user['results'][0]
-    processed_user = json_normalize({
-        'firstname': user['name']['first'],
-        'lastname': user['name']['last'],
-        'country': user['location']['country'],
-        'username': user['login']['username'],
-        'password': user['login']['password'],
-        'email': user['email'] })
-    processed_user.to_csv('/tmp/processed_user.csv', index = None, header = False) ## Adjust
-
+def _write_id_to_local_storage_file(ti):
+    _id = ti.xcom_pull(key = "my_key", task_ids = "choose_random_customer_id") 
+    file = open(os.path.join(local_storage_folder_name + local_storage_file_name, 'w')
+    file.write(_id) 
+    file.close() ## Adjust
+    
 # Defining variables to be used
-SPARK_JOB_PAYLOAD = {
+local_storage_folder_name = "/tmp/"
+remote_storage_folder_name = "REMOTE_LOCAL_PATH"
+local_storage_file_name = "lcustomer.txt"
+remote_storage_file_name = "acustomer.txt"
+local_storage_file_path = os.environ.get(local_storage_folder_name, local_storage_file_name)
+remote_storage_file_path = os.environ.get(remote_storage_folder_name, remote_storage_file_name)
+spark_job_payload = {
     "name": "SparkJob",
     "file": "abfss://spark@providersstorageaccgen2.dfs.core.windows.net/wordcount.py",
     "args": [
@@ -55,15 +57,24 @@ with DAG(
     choose_random_customer_id = PythonOperator(
         task_id = 'choose_random_customer_id',
         python_callable = _choose_random_customer_id
-    )
+    ) ## Adjust
     
-    tst = PythonOperator(task_id = 'tst', python_callable = _tst) ## Adjust
+    write_id_to_local_storage_file = PythonOperator(
+        task_id = 'write_id_to_local_storage_file', 
+        python_callable = _write_id_to_local_storage_file
+    ) ## Adjust
+
+    transfer_local_storage_file_to_azure_blob_storage = LocalFilesystemToADLSOperator(
+        task_id = "transfer_local_storage_file_to_azure_blob_storage",
+        local_path = local_storage_file_path,
+        remote_path = remote_storage_file_path
+    ) ## Adjust
 
     run_spark_job = AzureSynapseRunSparkBatchOperator(
                         task_id = "run_spark_job", 
                         spark_pool = "provsparkpool", 
                         payload = SPARK_JOB_PAYLOAD  ## Adjust
-                    )
+    ) ## Adjust
 
     # Defining the flow
     create_table >> is_api_available >> extract_user >> process_user >> store_user ## Adjust
