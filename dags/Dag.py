39112d191ed18ad2444cea6be3734f15be1877b1
Simão Machado <simaopedrotoga@gmail.com>
2024-04-29 19:57:35 +0100

    Content

--- dags/Dag.py
+++ dags/Dag.py
@@ -7,42 +7,41 @@ from airflow.providers.microsoft.azure.transfers.local_to_adls import LocalFiles
 from airflow.providers.microsoft.azure.operators.synapse import AzureSynapseRunSparkBatchOperator
 
 # Defining functions to be used
-def _choose_random_customer_id(ti):
+def _choose_feature_random_value(ti):
     ti.xcom_push(key = "my_key", value = "42") # Adjust
 
-def _write_id_to_local_storage_file(ti):
-    _id = ti.xcom_pull(key = "my_key", task_ids = "choose_random_customer_id") 
-    file = open(os.path.join(local_storage_folder_name + local_storage_file_name, 'w')
-    file.write(_id) 
+def _write_feature_random_value_to_local_storage_file(ti):
+    feature_random_value = ti.xcom_pull(key = "my_key", task_ids = "_choose_feature_random_value") 
+    file = open(os.path.join(local_storage_folder_name + local_remote_storage_file_name, 'w')
+    file.write(feature_random_value)
     file.close() ## Adjust
     
 # Defining variables to be used
 local_storage_folder_name = "/tmp/"
-remote_storage_folder_name = "REMOTE_LOCAL_PATH"
-local_storage_file_name = "lcustomer.txt"
-remote_storage_file_name = "acustomer.txt"
-local_storage_file_path = os.environ.get(local_storage_folder_name, local_storage_file_name)
-remote_storage_file_path = os.environ.get(remote_storage_folder_name, remote_storage_file_name)
-spark_job_payload = {
-    "name": "SparkJob",
-    "file": "abfss://spark@providersstorageaccgen2.dfs.core.windows.net/wordcount.py",
-    "args": [
-        "abfss://spark@providersstorageaccgen2.dfs.core.windows.net/shakespeare.txt",
-        "abfss://spark@providersstorageaccgen2.dfs.core.windows.net/results/",
-    ],
-    "jars": [],
-    "pyFiles": [],
-    "files": [],
-    "conf": {
-        "spark.dynamicAllocation.enabled": "false",
-        "spark.dynamicAllocation.minExecutors": "1",
-        "spark.dynamicAllocation.maxExecutors": "2",
-    },
-    "numExecutors": 2,
-    "executorCores": 4,
-    "executorMemory": "28g",
-    "driverCores": 4,
-    "driverMemory": "28g",
+remote_storage_folder_name = "abfss://dag@storageaccountnamedag.dfs.core.windows.net/synapse/workspaces/workspacenamedag/batchjobs/Spark%20job%20definition%201/"
+local_remote_storage_file_name = "FeatureValue.txt"
+local_storage_file_path = os.environ.get(local_storage_folder_name, local_remote_storage_file_name)
+remote_storage_file_path = os.environ.get(remote_storage_folder_name, local_remote_storage_file_name)
+spark_job = {
+                "name": "SparkJobWithModelPrediction",
+                "file": remote_storage_folder_name + "SparkJobWithModelPrediction.py",
+                "args": [
+                    remote_storage_folder_name + "FeatureValue.txt",
+                    remote_storage_folder_name, # ???
+                ],
+                "jars": [],
+                "pyFiles": [],
+                "files": [],
+                "conf": {
+                    "spark.dynamicAllocation.enabled": "false",
+                    "spark.dynamicAllocation.minExecutors": "1",
+                    "spark.dynamicAllocation.maxExecutors": "2",
+                },
+                "numExecutors": 2,
+                "executorCores": 4,
+                "executorMemory": "28g",
+                "driverCores": 4,
+                "driverMemory": "28g",
 } ## Adjust
 
 # Creating scheduled DAGs
@@ -54,27 +53,28 @@ with DAG(
      ) as dag: ## Adjust
 
     # Creating tasks/operators
-    choose_random_customer_id = PythonOperator(
-        task_id = 'choose_random_customer_id',
-        python_callable = _choose_random_customer_id
+    choose_feature_random_value = PythonOperator(
+        task_id = 'choose_feature_random_value',
+        python_callable = _choose_feature_random_value
     ) ## Adjust
     
-    write_id_to_local_storage_file = PythonOperator(
-        task_id = 'write_id_to_local_storage_file', 
-        python_callable = _write_id_to_local_storage_file
+    write_feature_random_value_to_local_storage_file = PythonOperator(
+        task_id = 'write_feature_random_value_to_local_storage_file', 
+        python_callable = _write_feature_random_value_to_local_storage_file
     ) ## Adjust
 
     transfer_local_storage_file_to_azure_blob_storage = LocalFilesystemToADLSOperator(
         task_id = "transfer_local_storage_file_to_azure_blob_storage",
         local_path = local_storage_file_path,
-        remote_path = remote_storage_file_path
+        remote_path = remote_storage_file_path,
+        overwrite = True
     ) ## Adjust
 
-    run_spark_job = AzureSynapseRunSparkBatchOperator(
-                        task_id = "run_spark_job", 
-                        spark_pool = "provsparkpool", 
-                        payload = SPARK_JOB_PAYLOAD  ## Adjust
+    run_azure_synapse_spark_job_with_model_prediction = AzureSynapseRunSparkBatchOperator(
+                        task_id = "run_azure_synapse_spark_job_with_model_prediction", 
+                        spark_pool = "sparknamedag", 
+                        payload = spark_job  ## Adjust
     ) ## Adjust
 
     # Defining the flow
-    create_table >> is_api_available >> extract_user >> process_user >> store_user ## Adjust
+    choose_feature_random_value >> write_feature_random_value_to_local_storage_file >> transfer_local_storage_file_to_azure_blob_storage >> run_azure_synapse_spark_job_with_model_prediction ## Adjust
