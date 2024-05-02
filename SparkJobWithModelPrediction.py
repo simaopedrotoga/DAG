00997f24c3737693ae8ed33f4de70a84a6847358
Simão Machado <simaopedrotoga@gmail.com>
2024-05-02 19:57:24 +0100

    Content

--- SparkJobWithModelPrediction.py
+++ SparkJobWithModelPrediction.py
@@ -1,16 +1,19 @@
 # Importing packages
 import numpy as np
 import pandas as pd
-from sklearn.datasets import load_boston
+from sklearn.datasets import fetch_california_housing
 from sklearn import linear_model
 
+# Import feature value
+feature_value = 0
+
 # Machine Learning
-boston = load_boston()
-dataset = pd.DataFrame(boston.data, columns = boston.feature_names)
-dataset['target'] = boston.target
-model = linear_model.LinearRegression(normalize = False, fit_intercept = True)
+housing = fetch_california_housing()
+dataset = pd.DataFrame(housing.data, columns = housing.feature_names)
+dataset['target'] = housing.target
+model = linear_model.LinearRegression(fit_intercept = True)
 num_observ = len(dataset)
-x = dataset['RM'].values.reshape((num_observ, 1))
+x = dataset['MedInc'].values.reshape((num_observ, 1))
 y = dataset['target'].values
 model.fit(x, y)
-print(model.predict("Feature value to be predicted")) # X deve sempre ser uma matriz e nunca um vetor
+print(model.predict(np.asarray([[feature_value]])))
