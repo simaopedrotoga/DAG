3a954ad3e9b9f2f8a67758b3b1ed01f778484eae
Simão Machado <simaopedrotoga@gmail.com>
2024-05-09 20:51:50 +0100

    Content

--- SparkJobWithModelPrediction.py
+++ SparkJobWithModelPrediction.py
@@ -7,6 +7,7 @@ from sklearn import linear_model
 # Import feature value
 remote_storage_folder_name = "abfss://dag@storageaccountnamedag.dfs.core.windows.net/synapse/workspaces/workspacenamedag/batchjobs/sparkjobdefinition1/"
 local_remote_storage_file_name = "FeatureValue.txt"
+remote_storage_file_name = "PredictedValue.txt"
 remote_storage_file_path = remote_storage_folder_name + local_remote_storage_file_name
 feature_value = int(pd.read_csv(remote_storage_file_path).columns[0])
 
@@ -19,4 +20,5 @@ num_observ = len(dataset)
 x = dataset['MedInc'].values.reshape((num_observ, 1))
 y = dataset['target'].values
 model.fit(x, y)
-print(model.predict(np.asarray([[feature_value]])))
+predicted_value = float(model.predict(np.asarray([[feature_value]]))[0])
+pd.DataFrame({"Predicted Value": [predicted_value]}).to_csv(remote_storage_folder_name + remote_storage_file_name, index = False)
